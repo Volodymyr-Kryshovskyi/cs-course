00c5c8fae8f04de0538bd3cc6eba8615a3a8575f
Volodymyr-Kryshovskyi <trplovich6@gmail.com>
2025-10-21 14:44:13 +0200

    change file loop-for to loop-for-and-foreach

--- arrays-loops/loop-for-and-foreach.cs
+++ arrays-loops/loop-for-and-foreach.cs
@@ -10,13 +10,23 @@ public class script : MonoBehaviour
 
   //private int number;
   [SerializeField] float[] numberArray = { 50.50f, 20.20f, 40.23f, 60.79f, 20.23f , 49.001f }; // float or int
-    private void Start()
+  private void Start()
+  {
+    for (int i = 0; i <= numberArray.Length - 2; i++)
     {
-          for (int i = 0; i <= numberArray.Length-2; i++)
-          {
-             Debug.Log(numberArray[i].ToString("F2"));
-          }
+      Debug.Log(numberArray[i].ToString("F2"));
     }
+
+
+
+    foreach (var numberr in numberArray)
+    {
+      Debug.Log(numberr);
+    }
+  }
+
+    
+    
 
    
    
