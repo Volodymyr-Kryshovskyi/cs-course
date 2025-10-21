3af869a39c2d29a0f28b2242bd647ae00d0d128d
Volodymyr-Kryshovskyi <trplovich6@gmail.com>
2025-10-21 15:48:21 +0200

    Add two file

---
+++ classes/ClassLesson.cs
@@ -0,0 +1,17 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+[Serializable]
+public class ClassLesson 
+{
+    public int money;
+    public float honey;
+    public bool sunny;
+    public bool vKarmany;
+      
+    
+
+
+}
---
+++ classes/script.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+
+public class script : MonoBehaviour
+{
+
+  
+     [SerializeField] private int a;
+    [SerializeField] private int b;
+
+    [SerializeField] private ClassLesson[] classLesson;
+
+    private void  Start()
+    {
+        Debug.Log(a + b);
+        
+    }
+      
+
+   
+}
+
+  
+
+
+   
+
+
