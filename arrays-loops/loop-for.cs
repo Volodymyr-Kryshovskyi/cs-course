bb3d8cd344a8d51d6745dae5323ddfa2361bc0f4
Volodymyr-Kryshovskyi <trplovich6@gmail.com>
2025-10-21 14:39:59 +0200

    add file loop-for

---
+++ arrays-loops/loop-for.cs
@@ -0,0 +1,38 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+
+public class script : MonoBehaviour
+{
+
+  //private int number;
+  [SerializeField] float[] numberArray = { 50.50f, 20.20f, 40.23f, 60.79f, 20.23f , 49.001f }; // float or int
+    private void Start()
+    {
+          for (int i = 0; i <= numberArray.Length-2; i++)
+          {
+             Debug.Log(numberArray[i].ToString("F2"));
+          }
+    }
+
+   
+   
+  //   Debug.Log("Start");
+  //   while (number < 5)
+  //   {
+      
+  //     number++;
+  //     Debug.Log("Молодец твои очки :" + number);
+  //   }
+  //    Debug.Log("Finish");
+}
+
+  
+
+
+   
+
+
